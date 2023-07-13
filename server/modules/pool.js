9c815d6f4c5a18fec6d32b133a1e3f2bd74f3b50
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-13 00:12:55 -0500

    deploy to heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,21 @@
+// require pg
 const pg = require('pg');
+// set up pg to connect to database 
+let pool; 
 
-const pool = new pg.Pool({
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL, 
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+}else {
+    pool = new pg.Pool({
+    database: 'weekend-gallery',
     host: 'localhost',
-    port: 5432,
-    database: 'weekend-gallery', 
+    port: 5432
 });
-
-module.exports = pool;
+};
+// exporting pool for use in server
+module.exports = pool
