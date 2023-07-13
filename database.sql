55878ee54fc58e594978c6373dd488eed8802fe6
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-12 23:27:12 -0500

    fixed database

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+-- Name of database: "weekend-gallery"
+
 CREATE TABLE weekend_gallery (
     id SERIAL PRIMARY KEY,
     alt VARCHAR(800),
@@ -6,6 +8,6 @@ CREATE TABLE weekend_gallery (
     likes INTEGER DEFAULT 0
 );
 
+-- Test data 
 INSERT INTO weekend_gallery (alt, path, description)
 VALUES ('Cheeto', 'images/cheeto.jpeg', 'Cheeto is cuddly and mellow'), ('Eva', 'images/eva.jpeg', 'Eva is shy'),  ('Kuma', 'images/kuma.jpeg', 'Kuma is shy and socially awkward'),  ('Petunia', 'images/petunia.jpeg', 'Petunia is sweet'),  ('Roxie', 'images/roxie.jpeg', 'Roxie is smart'),  ('Thor', 'images/thor.jpeg', 'Thor is chonky');
-
