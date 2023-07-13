1487a402dca38f0bb8fb75d8e0ccdd5f18785aa9
Quynh Ngan Dao <nguy2884@umn.edu>
2023-07-12 22:23:03 -0500

    fixed multer issues

--- server/routes/single.router.js
+++ server/routes/single.router.js
@@ -2,28 +2,34 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 const multer = require("multer");
+router.use(express.static(__dirname + "public"));
+router.use("/images", express.static("images"));
 
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/images");
+    // specify the directory to save the uploaded files
+    cb(null, "public/images");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '--' + file.originalname);
+    cb(null, file.originalname);
   },
 });
+
 const upload = multer({ storage: fileStorageEngine });
 
-router.post("/", upload.single("image"), (req, res) => {
+router.post("/", upload.single("image"), function (req, res, next) {
+  // req.body will hold the text fields
   const { alt, description } = req.body;
-  const imagePath = req.file.filename;
-    const queryText = `INSERT INTO "weekend_gallery" (alt, path, description) VALUES ($1, $2, $3);`;
+
+  // req.file is the `image` file
+  console.log("req.file", req.file);
+
+  const queryText = `INSERT INTO "weekend_gallery" (alt, description) VALUES ($1, $2);`;
   pool
-    .query(queryText, [alt, imagePath, description])
-    .then((result) => {
-      console.log('req.file', req.file);
-      console.log('req.body', req.body);
-      console.log("Added item to database", result);
-      res.sendStatus(201);
+    .query(queryText, [alt, description])
+    .then((response) => {
+      res.status(201);
+      console.log("Added item to database", response);
     })
     .catch((error) => {
       console.log("Error adding item to database", error);
